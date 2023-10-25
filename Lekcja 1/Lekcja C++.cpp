478d7967c0465c96c3a9c48b1ba81caa52a37785
WiktorLandowski/Informatyka <wwiktorland@gmail.com>
2023-10-25 14:11:26 +0200

    Komentarz

---
+++ Lekcja 1/Lekcja C++.cpp
@@ -0,0 +1,21 @@
+ #include <iostream> 
+ using namespace std;
+ int main (){ 
+ 
+ short liczba = 2;
+ float zmiennoprzecinkowa = 3.532;
+ string tekst  = "tekst";
+ char znak = 'A';
+ bool znakowy = false;
+ 
+ cout << "hello world" << endl;
+ cout << "Liczba calkowita: " << liczba;
+ cout << endl;
+ string jakos;
+ cout << "Podaj jakis wyraz: ";
+ cin >> jakos;
+ cout << "sprawdziles warosc: " << jakos;
+ 
+ 
+  return 0;
+ }
