61a3cd91478163b4e6716c54c6d1566566b54891
WiktorLandowski/Informatyka <wwiktorland@gmail.com>
2023-11-16 15:02:50 +0100

    Komentarz

---
+++ Lekcja 5/Petla for zadanie 3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int szerokosc, wysokosc;
+ 	cout << "Podaj szerokosc " << endl;
+ 	cin >> szerokosc;
+ 	cout << "Podaj wysokosc " << endl;
+ 	cin >> wysokosc;
+ 	
+ 	for(int i=0; i < wysokosc; i++){
+		for(int j=0; j < szerokosc; j++){
+			cout << "*";
+			
+ }
+ cout << endl;
+}
+}
