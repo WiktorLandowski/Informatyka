cb5c8debbd3d4c216d08e1fb3b63052a86858712
WiktorLandowski/Informatyka <wwiktorland@gmail.com>
2023-12-07 14:56:28 +0100

    Komentarz

---
+++ Tablice C++/Tablice 4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+const int wiersze = 3;
+const int kolumny = 5;
+int tablica[wiersze][kolumny];
+int liczba = 1;
+for(int i = 0; i < wiersze; i++){
+	for(int j =0; j < kolumny; j++){
+		
+	}
+	
+	
+	for(int i = 0; i < wiersze; i++){
+		for(int j = 0; j < kolumny; j++){
+		}
+	}
+	cout << tablica[i] << " ";
+}
+cout << endl;
+		
+}
+	
