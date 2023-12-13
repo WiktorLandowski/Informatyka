f7b6a34e7dd668ece318129f5ab48caf0da24814
WiktorLandowski/Informatyka <wwiktorland@gmail.com>
2023-12-13 14:06:57 +0100

    Komentarz

--- Tablice Znakowe/Teblice - symulacja.cpp
+++ Tablice Znakowe/Teblice - symulacja.cpp
@@ -3,20 +3,11 @@ using namespace std;
 
 int main()
 {
-    //pierwszy sposób
-    char tab[5] = {'a','b','\0'}; //tworzymy tablicę 5-elementową, która może przechować do 4 znaków
-
-    cout<< tab<< endl;
-    
-      //drugi sposób - nie podając wielkości tablicy - program sam dopasuje jej wielkość
-    char tab2[] = {'a','b','\0'}; //tym razem tworzymy tablicę 3-elementową
-
-    cout<< tab2<< endl; 
-
-    //trzeci sposób - podajemy ciąg znaków pamiętając o podwójnym cudzysłowie
-    char tab3[] = "Ala ma kota :)";
-
-    cout<< tab3<< endl; //przypisanie do każdego elementu tablicy pojedyńczego znakku
+    char tablica[100];
+    cout << "Podaj imie i nazwisko: "; 
+    cin >> tablica;
+    cout << "Twoje dane osobowe: "<< tablica << endl; //dane zostaną zredukowane do imienia
+    system("pause");
 
     return 0;
 }
