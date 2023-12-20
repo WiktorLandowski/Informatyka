ae9eeb4679630699f607f858b90436661d6104af
WiktorLandowski/Informatyka <wwiktorland@gmail.com>
2023-12-20 14:03:38 +0100

    Komentarz

---
+++ Tablice Znakowe/Tablice Znakowe Zad.2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+char text[201];
+cout << "Wprowadz tekst (maks 200 znakow): ";
+cin.getline(text, 201);
+for (int i=0; text[i] != '\0'; i++){
+	if (text[i] == ' ' = '-') (text[i] == '.' = '-');{
+			}
+cout << "Zmieniony tekst: " << text[i] << endl;
+return 0;
+}
+}
