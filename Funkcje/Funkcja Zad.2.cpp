20af6e76eafe102708845fd94a167a01e600096f
WiktorLandowski/Informatyka <wwiktorland@gmail.com>
2024-01-04 14:59:25 +0100

    Komentarz

---
+++ Funkcje/Funkcja Zad.2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+const float TAX_RATE = 0.23;
+float Brutto(float netto) {
+    return netto * (1 + TAX_RATE);
+}
+float Netto(float brutto) {
+    return brutto / (1 + TAX_RATE);
+}
+
+int main()
+{
+    int wybor;
+    float netto, brutto;
+    cout << "Ktora kwote chcesz obliczyc? \n 1. Brutto \n 2. Netto" << endl;
+    cin >> wybor;
+
+    switch(wybor)
+    {
+        case 1:
+            cout << "Podaj kwote netto: ";
+            cin >> netto;
+            cout << "Brutto: " << Brutto(netto) << " zl" << endl;
+            break;
+        case 2:
+            cout << "Podaj kwote brutto: ";
+            cin >> brutto;
+            cout << "Netto: " << Netto(brutto) << " zl" << endl;
+            break;
+    }
+}
