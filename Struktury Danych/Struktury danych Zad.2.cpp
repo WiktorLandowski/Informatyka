c18edded0b2878c990206486a354a6fdbc8ec6c6
WiktorLandowski/Informatyka <wwiktorland@gmail.com>
2024-03-18 13:52:24 +0100

    Komentarz

--- Struktury Danych/Struktury danych Zad.2.cpp
+++ Struktury Danych/Struktury danych Zad.2.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
+using namespace std;
  
 struct Ksiazka {
-    std::string tytul;
-    std::string autor;
+    string tytul;
+    string autor;
     int rokWydania;
-    std::string wydawnictwo;
+    string wydawnictwo;
     double cena;
 };
  
@@ -23,35 +24,37 @@ int main() {
     };
  
     while (true) {
-        std::cout << "Witaj w ksiegarnii\nPo jakiej kategorii chcial bys wyszukac ksiazke:\n1. Rok wydania\n2. Cena\n3. Wydawnictwo\n0. Wyjscie\n";
+        cout << "Witaj w ksiegarnii\nPo jakiej kategorii chcial bys wyszukac ksiazke:\n1. Rok wydania\n2. Cena\n3. Wydawnictwo\n0. Wyjscie\n";
         int opcja;
-        std::cin >> opcja;
-        if (opcja == 0) break;
+        cin >> opcja;
+ 
+        if (opcja == 0)
+            break;
  
         switch (opcja) {
             case 1: {
                 int rok;
-                std::cout << "Podaj od jakiego roku chcesz zobaczyc ksiazki: ";
-                std::cin >> rok;
+                cout << "Podaj od jakiego roku chcesz zobaczyc ksiazki: ";
+                cin >> rok;
                 wyszukajKsiazki(ksiazki, 7, opcja, &rok);
                 break;
             }
             case 2: {
                 double cena;
-                std::cout << "Podaj minimalna cene ksiazki: ";
-                std::cin >> cena;
+                cout << "Podaj minimalna cene ksiazki: ";
+                cin >> cena;
                 wyszukajKsiazki(ksiazki, 7, opcja, &cena);
                 break;
             }
             case 3: {
-                std::string wydawnictwo;
-                std::cout << "Podaj nazwe wydawnictwa: ";
-                std::cin >> wydawnictwo;
+                string wydawnictwo;
+                cout << "Podaj nazwe wydawnictwa: ";
+                cin >> wydawnictwo;
                 wyszukajKsiazki(ksiazki, 7, opcja, &wydawnictwo);
                 break;
             }
             default:
-                std::cout << "Nieznana opcja\n";
+                cout << "Nieznana opcja\n";
                 break;
         }
     }
@@ -61,29 +64,40 @@ int main() {
  
 void wyszukajKsiazki(const Ksiazka ksiazki[], int rozmiar, int opcja, const void* kryterium) {
     bool znaleziono = false;
-
+ 
     for (int i = 0; i < rozmiar; ++i) {
         bool pasuje = false;
+ 
         switch (opcja) {
             case 1: {
                 int rok = *(int*)kryterium;
-                if (ksiazki[i].rokWydania >= rok) pasuje = true;
+                if (ksiazki[i].rokWydania >= rok)
+                    pasuje = true;
                 break;
             }
             case 2: {
                 double cena = *(double*)kryterium;
-                if (ksiazki[i].cena >= cena) pasuje = true;
+                if (ksiazki[i].cena >= cena)
+                    pasuje = true;
                 break;
             }
             case 3: {
-                std::string wydawnictwo = *(std::string*)kryterium;
-                if (ksiazki[i].wydawnictwo == wydawnictwo) pasuje = true;
+                string wydawnictwo = *(std::string*)kryterium;
+                if (ksiazki[i].wydawnictwo == wydawnictwo)
+                    pasuje = true;
                 break;
             }
-}
+        }
+ 
         if (pasuje) {
             znaleziono = true;
-            std::cout << "Tytul:" << ksiazki[i].tytul << "Autor:" << ksiazki[i].autor << "Rok:" << ksiazki;
+            cout << "Tytul: " << ksiazki[i].tytul << "\nAutor: " << ksiazki[i].autor
+                      << "\nRok: " << ksiazki[i].rokWydania << "\nWydawnictwo: " << ksiazki[i].wydawnictwo
+                      << "\nCena: " << ksiazki[i].cena << "\n\n";
         }
+    }
+ 
+    if (!znaleziono) {
+        cout << "Brak pasujacych ksiazek.\n\n";
     }
 }
